16c2393fbc3e7053f0ed63b3fd7becbe367d1b1c
ravana <ravanabrahma@gmail>
2024-02-08 15:11:52 +0530

    Initial commit

---
+++ 1st_program.py
@@ -0,0 +1,7 @@
+#1.Write a python program to count the total numberof charecters in the given string?
+#Ravanabrahma
+st = input('Enter Any String: ')
+count = 0
+for i in st:
+    count = count+1
+print(count)  
